b32be1a4149cdc40acf5a80f69e70c337fc0d994
dashaborzova <dashaborzova@yandex.ru>
2024-04-09 21:29:18 +0300

    feat: pass 4 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -172,8 +172,9 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const fn2 = (...args2) => fn(...args1, ...args2);
+  return fn2;
 }
 
 /**
@@ -193,8 +194,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+  return function getNextId() {
+    const nextId = currentId;
+    currentId += 1;
+    return nextId;
+  };
 }
 
 module.exports = {
